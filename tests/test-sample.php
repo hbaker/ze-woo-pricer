b99a89ab1d63e832a3c37e2480fc3da5dc363882
Hosea Baker <hosea.baker@gmail.com>
2017-03-14 01:09:35 -0500

    Initial plugin scaffolding with unit testing.

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Ze_Woo_Pricer
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
---
+++ ze-woo-pricer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Ze Woo Pricer
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ze-woo-pricer
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ze_Woo_Pricer
+ */
