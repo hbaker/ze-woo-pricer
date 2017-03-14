8971feb633ec97efad62d92795f0791cf214dd06
Hosea Baker <hosea.baker@gmail.com>
2017-03-14 02:51:06 -0500

    Add "per square foot" after price

--- ze-woo-pricer.php
+++ ze-woo-pricer.php
@@ -15,13 +15,20 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @package         Ze_Woo_Pricer
  */
 
-/* CHECK IF WOOCOMMERCE IS ACTIVE */
-/* CHECK IF WOOCOMMERCE IS ACTIVE */
- **/
+// CHECK IF WOOCOMMERCE IS ACTIVE
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
-    
+    // ADD "PER SQUARE FOOT" AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
+    // ADD "PER SQUARE FOOT" AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
+    function ze_change_price_display( $price ) {
+      $price .= ' per square foot';
+      return $price;
+    }
+    add_filter( 'woocommerce_get_price_html', 'ze_change_price_display' );
+    add_filter( 'woocommerce_cart_item_price', 'ze_change_price_display' );
+    // END ADD "PER SQUARE FOOT" AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
+    // END ADD "PER SQUARE FOOT" AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
 
 }
-/* END CHECK IF WOOCOMMERCE IS ACTIVE */
-/* END CHECK IF WOOCOMMERCE IS ACTIVE */
+// END CHECK IF WOOCOMMERCE IS ACTIVE
+// END CHECK IF WOOCOMMERCE IS ACTIVE
