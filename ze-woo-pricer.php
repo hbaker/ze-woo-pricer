9d9166d5815c8102569d608245910bcf2c7c3e85
Hosea Baker <hosea.baker@gmail.com>
2017-03-14 02:11:25 -0500

    Revert "Read Update"
    This reverts commit db2366c10aab5542be03569716222a9f6dab447e.

--- ze-woo-pricer.php
+++ ze-woo-pricer.php
@@ -1,31 +1,13 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) { 
-    exit; // Exit if accessed directly
-}
 /**
  * Plugin Name:     Ze Woo Pricer
- * Plugin URI:      
- * Description:     A price per square footage plugin for WooCommerce
- * Author:          Hosea Baker
- * Author URI:      https://hoseabaker.com
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
  * Text Domain:     ze-woo-pricer
  * Domain Path:     /languages
  * Version:         0.1.0
  *
  * @package         Ze_Woo_Pricer
  */
-
-/**
- * CHECK IF WOOCOMMERCE IS ACTIVE
- **/
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-    
-    // ADD "PER SQ. FT." AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
-    function ze_change_price_display( $price ) {
-      $price .= ' per square foot';
-      return $price;
-    }
-    add_filter( 'woocommerce_get_price_html', 'ze_change_price_display' );
-    add_filter( 'woocommerce_cart_item_price', 'ze_change_price_display' );
-
-} /* END CHECK IF WOOCOMMERCE IS ACTIVE */
