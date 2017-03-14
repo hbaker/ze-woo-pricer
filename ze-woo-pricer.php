546464e85ded05998f4c7c50fdd6205011bf27d4
Hosea Baker <hosea.baker@gmail.com>
2017-03-14 02:48:46 -0500

    Revert "ADDED "PER SQUARE FOOT" AFTER PRICE"
    This reverts commit 56cd6ac4272c7bfe9be7fefaf2bd39b16d9ed4e0.

--- ze-woo-pricer.php
+++ ze-woo-pricer.php
@@ -20,16 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  **/
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
-    // ADD "PER SQUARE FOOT" AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
-    // ADD "PER SQUARE FOOT" AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
-    function sv_change_product_price_display( $price ) {
-      $price .= ' per package';
-      return $price;
-    }
-    add_filter( 'woocommerce_get_price_html', 'sv_change_product_price_display' );
-    add_filter( 'woocommerce_cart_item_price', 'sv_change_product_price_display' );
-    // END ADD "PER SQUARE FOOT" AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
-    // END ADD "PER SQUARE FOOT" AFTER EACH PRODUCT PRICE THROUGHOUT THE SHOP
+    
 
 }
 /* END CHECK IF WOOCOMMERCE IS ACTIVE */
