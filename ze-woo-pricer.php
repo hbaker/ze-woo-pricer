c0688e1306b96c1aba89d2b380463ffe7b6f3d9f
Hosea Baker <hosea.baker@gmail.com>
2017-03-14 02:19:28 -0500

    ADDED DATA LEAK PROTECTION

--- ze-woo-pricer.php
+++ ze-woo-pricer.php
@@ -1,10 +1,13 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) { 
+    exit; // Exit if accessed directly
+}
 /**
  * Plugin Name:     Ze Woo Pricer
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      
+ * Description:     A price per square footage plugin for WooCommerce
+ * Author:          Hosea Baker
+ * Author URI:      https://hoseabaker.com
  * Text Domain:     ze-woo-pricer
  * Domain Path:     /languages
  * Version:         0.1.0
